b92b15cea15d4f5ad35dab13416da734e015a5a4
NKS01X <nikhilayush2020@gmail.com>
2026-03-31 19:49:14 +0530

    added yaml-config setup for scaling up and scaling down

--- demon/demon.go
+++ demon/demon.go
@@ -2,18 +2,54 @@ package demon
 
 import (
 	"fmt"
+	"os"
 
 	lb "vortex/loadbalancer"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 )
+
+type VortexConfig struct {
+	Cluster struct {
+		MinReplicas  int `yaml:"min_replicas"`
+		MaxReplicas  int `yaml:"max_replicas"`
+		StartingPort int `yaml:"starting_port"`
+	} `yaml:"cluster"`
+	Settings struct {
+		Scaleupnumber   int `yaml:"scaleupnumber"`
+		Scaledownnumber int `yaml:"scaledownnumber"`
+	} `yaml:"settings"`
+}
+
+var config VortexConfig
+
+func ParseYamlFile() {
+	yamlFile, err := os.ReadFile("vortex.yaml")
+	if err != nil {
+		fmt.Printf("Fatal error: Could not find vortex.yaml: %v\n", err)
+		return
+	}
+
+	yaml.Unmarshal(yamlFile, &config)
+	fmt.Printf("Config loaded! Starting Port: %d, Initial Nodes: %d\n", config.Cluster.StartingPort, config.Cluster.MinReplicas)
+	fmt.Println("========================================")
+	// SetConfig(config)
+	AddServers(config.Cluster.MinReplicas)
+}
 
 // this file works with the adding and removing of backends dynamically
+var InitialPort = config.Cluster.StartingPort
 
-var (
-	InitialPort = 3001
-	InitialUrl  = "http://127.0.0.1:"
-)
+var InitialUrl = "http://127.0.0.1:"
+
+// var MaxReplica int
+// var MinReplica int
+//
+// var ServerConfig VortexConfig
+// func SetConfig(config VortexConfig) {
+// 	ServerConfig = config
+// }
 
 /*
  * @params = amount of server to spin up
@@ -42,5 +78,21 @@ func AddServers(x int) {
 	InitialPort += x
 }
 
+// func RemoveServers(x int) {
+// 	for i := 0; i < x; i++ {
+// 	}
+// }
+
+var CurrentServers uint64 = 0
+
 func Demon() {
+	// r := gin.Default()
+	// go r.GET("/", func(c *gin.Context) {
+	// 	CurrentServers++
+	// 	if(CurrentServers >= uint64(config.Settings.Scaleupnumber)) {
+	// 		AddServers(1)
+	// 	}else if(CurrentServers <= uint64(config.Settings.Scaledownnumber)) {
+
+	// 	}
+	// })
 }
