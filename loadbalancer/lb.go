730c113ced54f425a3fe4c61f8fd7a35d49ea255
NKS01X <nikhilayush2020@gmail.com>
2026-03-31 18:13:13 +0530

    updated loadbalancer

--- loadbalancer/lb.go
+++ loadbalancer/lb.go
@@ -1,6 +1,12 @@
 package loadbalancer
 
-import "sync/atomic"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync/atomic"
+)
 
 type servers struct {
 	IP string
@@ -14,4 +20,17 @@ func getNextBackend() servers {
 	sz_of_backend := len(backend)
 	next := atomic.AddUint64(&count_request, 1)
 	return backend[(uint64(next)-1)%uint64(sz_of_backend)]
+}
+
+func Load_Balancer(w http.ResponseWriter, r *http.Request) {
+	targetUrl := getNextBackend()
+	fmt.Printf("Routing the reponse to = %s\n", targetUrl)
+	target, err := url.Parse(targetUrl.IP)
+	if err != nil {
+		http.Error(w, "Server Error", http.StatusInternalServerError)
+		return
+	}
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	r.Host = target.Host
+	proxy.ServeHTTP(w, r)
 }
